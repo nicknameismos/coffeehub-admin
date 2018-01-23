3c9e27fab9ac852b8d971c619fe41aa4610f8199
VAMPIRE-MICK <nutprapobsun.mick@gmail.com>
2018-01-23 10:29:36 +0700

    build

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -4643,7 +4643,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 var ServerConfig = (function () {
     function ServerConfig() {
-        this.url = 'https://eatsyd-test.herokuapp.com/';
+        this.url = 'https://coffeehub-server.herokuapp.com/';
     }
     ServerConfig.prototype.isLogin = function () {
         var user = window.localStorage.getItem('user');
